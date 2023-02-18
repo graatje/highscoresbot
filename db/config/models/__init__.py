1eeeddc883245b57a3515cf2dc765eebc670d9ce
KevinPoelstra <k.poelstra@zorgfocuz.nl>
2023-02-18 18:01:19 +0100

    Removed db folder

--- db/config/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from api.ingame_data.models.eventname import Eventname
-from api.ingame_data.models.tournamentprize import Tournamentprize
-from api.ingame_data.models.honeylocation import Honeylocation
-from api.ingame_data.models.worldbosslocation import Worldbosslocation
-
-
-__all__ = ["Eventname", "Tournamentprize", "Honeylocation", "Worldbosslocation"]
