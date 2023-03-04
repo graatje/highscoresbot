d47a12233fcda2c479a0a12256b6b4a413b2ef64
KevinPoelstra <k.poelstra@zorgfocuz.nl>
2023-03-04 20:03:01 +0100

    hotfix get clan of player

--- utils/getclanofplayer.py
+++ utils/getclanofplayer.py
@@ -2,4 +2,6 @@ from api.highscores.models import Highscore
 
 
 def getClanOfPlayer(playername):
-    return Highscore.objects.filter(data__username__iexact=playername, data__clan__isnull=False).first().data['clan']
+    obj = Highscore.objects.filter(data__username__iexact=playername, data__clan__isnull=False).first()
+    if obj:
+        return obj.data['clan']
