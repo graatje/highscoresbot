4ff3125c25a380229bf180f5338595e3d1b8e1e6
kevin <kevin.plstr@gmail.com>
2023-10-08 16:08:24 +0200

    fixed registering for events

--- commands/interractions/eventconfig/register.py
+++ commands/interractions/eventconfig/register.py
@@ -17,28 +17,20 @@ class Register(SelectsUtility):
     async def callback(self, interaction: discord.Interaction):
         if not await self.isOwner(interaction): return
         for event in self.values:
+
             func = sync_to_async(Eventname.objects.get)
             eventobj = await func(name=event)
             try:
-                createfunc = sync_to_async(Eventconfiguration.objects.create)
-                await createfunc(eventname=eventobj, guild=self.channel.guild.id, channel=self.channel.id)
-
-                await self.channel.send(
-                    f"This channel has been properly configured for sending the {event} event "
-                    f"{self.interaction.user.mention}!")
-                if not interaction.response.is_done():
-                    await interaction.response.send_message("success!")
-            except IntegrityError:
                 eventconfigfunc = sync_to_async(Eventconfiguration.objects.get)
                 eventconfig: Eventconfiguration = await eventconfigfunc(eventname=eventobj, guild=self.channel.guild.id)
                 eventconfig.channel = self.channel.id
                 await (sync_to_async(eventconfig.save))()
-                await self.channel.send(
+            except Eventconfiguration.DoesNotExist:
+                createfunc = sync_to_async(Eventconfiguration.objects.create)
+                await createfunc(eventname=eventobj, guild=self.channel.guild.id, channel=self.channel.id)
+
+            await self.channel.send(
                     f"This channel has been properly configured for sending the {event} event "
                     f"{self.interaction.user.mention}!")
-            except Exception as e:
-                if not interaction.response.is_done():
-                    await interaction.response.send_message("unknown error occured.")
-                else:
-                    await self.channel.send("unknown error occured.")
-                raise e
+            if not interaction.response.is_done():
+                await interaction.response.send_message("success!")
