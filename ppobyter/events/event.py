df16fb8c091ff8be0b86a7f07377d6ddf0dcf9b8
kevin <kevin.plstr@gmail.com>
2023-10-11 11:15:23 +0200

    removed the try-except for testing

--- ppobyter/events/event.py
+++ ppobyter/events/event.py
@@ -75,33 +75,30 @@ class Event:
                 print(e)
 
     async def __handle_failed_send(self, configuration, client):
-        try:
-            configuration.failed_sends += 1
-            if configuration.failed_sends <= 20:
-                await configuration.asave()
-                return
 
-            configuration.channel = None
-            configuration.failed_sends = 0
+        configuration.failed_sends += 1
+        if configuration.failed_sends <= 20:
             await configuration.asave()
-            print(f"removed channel from eventconfiguration because of too many failed sends.")
+            return
 
-            guild = await client.fetch_guild(configuration.guild)
+        configuration.channel = None
+        configuration.failed_sends = 0
+        await configuration.asave()
+        print(f"removed channel from eventconfiguration because of too many failed sends.")
 
-            if not guild:
-                return
+        guild = await client.fetch_guild(configuration.guild)
 
-            owner = await client.fetch_user(guild.owner_id)
-            if not owner:
-                return
+        if not guild:
+            return
 
+        owner = await client.fetch_user(guild.owner_id)
+        if not owner:
+            return
 
-            message = f"""
-    I lack permissions to send messages in <#{configuration.channel}> in {guild.name}!
-    Therefore i removed that channel from the eventconfiguration.
-    Please give me permissions to send messages in there and reconfigure the eventconfiguration for the {configuration.eventname} event.
-            """
+        message = f"""
+I lack permissions to send messages in <#{configuration.channel}> in {guild.name}!
+Therefore i removed that channel from the eventconfiguration.
+Please give me permissions to send messages in there and reconfigure the eventconfiguration for the {configuration.eventname} event.
+        """
 
-            await owner.send(message)
-        except Exception as e:
-            print("error during handling failed send:", e)
+        await owner.send(message)
