50d12ee8a7dd2eb330c0f844e9ca0251b0bfb53a
Kevin <k.poelstra@zorgfocuz.nl>
2023-10-29 22:33:27 +0100

    clan wars implemented.

--- ppobyter/events/worldblessing.py
+++ ppobyter/events/worldblessing.py
@@ -1,34 +1,27 @@
-import datetime
-import re
-from ppobyter.events.timedevent import TimedEvent
-
-
-class WorldBlessing(TimedEvent):
-    """
-    This event triggers 2 minutes before the expiring of the worldblessing.
-    """
-    def __init__(self):
-        """
-        calls the superclass init.
-        """
-        self.EVENTNAME = "worldblessing"
-        super(WorldBlessing, self).__init__(datetime.timedelta(minutes=30))
-
-    def messageProcesser(self, message: str):
-        pattern = r"(?<=<\/var><var n='d' t='s'>)([0-9]+:[0-9]+)(?=<\/var><var n='a' t='n'>1.1</var><var n='_cmd' t='s'>" \
-                  r"worldBlessing<\/var><\/dataObj>]]><\/body><\/msg>)"
-        if re.search(pattern, message):
-            print("activation time set.")
-            self.setActivationTime(datetime.datetime.now() + datetime.timedelta(minutes=57))
-
-    def makeMessage(self) -> str:
-        """
-        Makes the message that gets sent to the recipients.
-        :return: the message
-        """
-        return "world blessing expiring in 2 minutes!"
-
-    def __bool__(self):
-        now = datetime.datetime.now()
-        return self.activationtime is not None and not self.hasCooldown() and self.activationtime <= now <= \
-               (self.activationtime + datetime.timedelta(minutes=3))
+# import datetime
+# import re
+#
+#
+#
+# class WorldBlessing(TimedEvent):
+#     """
+#     This event triggers 2 minutes before the expiring of the worldblessing.
+#     """
+#     def __init__(self):
+#         """
+#         calls the superclass init.
+#         """
+#         self.EVENTNAME = "worldblessing"
+#         super(WorldBlessing, self).__init__(datetime.timedelta(minutes=30))
+#
+#     def makeMessage(self) -> str:
+#         """
+#         Makes the message that gets sent to the recipients.
+#         :return: the message
+#         """
+#         return "world blessing expiring in 2 minutes!"
+#
+#     def __bool__(self):
+#         now = datetime.datetime.now()
+#         return self.activationtime is not None and not self.hasCooldown() and self.activationtime <= now <= \
+#                (self.activationtime + datetime.timedelta(minutes=3))
--- ppobyter/events/worldboss.py
+++ ppobyter/events/worldboss.py
@@ -1,8 +1,4 @@
 import sqlite3
-import time
-import datetime
-
-import pyautogui
 from .event import Event
 
 
@@ -20,7 +16,6 @@ class Worldboss(Event):
         self.pokemon = pokemon.lower()
         self.EVENTNAME = "worldboss"
         super(Worldboss, self).__init__()
-        self.presspowerticket()
 
     def makeMessage(self) -> str:
         """
@@ -34,32 +29,3 @@ class Worldboss(Event):
         Determines pmrecipients and channelrecipients.
         """
         self._determinechannelrecipients()
-        self.__determinepmrecipients()
-
-    def presspowerticket(self):
-        """
-        presses the powerticket.
-        """
-        for i in range(2):
-            try:
-                pyautogui.click(300, 420, clicks=2)
-                time.sleep(0.2)
-            except Exception as e:
-                print(e)
-                print("failed to press ticket.")
-
-    def __determinepmrecipients(self):
-        """
-        Determines the pmrecipients based on worldboss pokemon, location or a combination of both.
-        """
-        conn = sqlite3.connect(self.pathManager.getpath("eventconfigurations.db"))
-        cur = conn.cursor()
-        cur.execute("SELECT playerid FROM pmworldboss WHERE location=? AND boss=? AND comparator='&'",
-                    (self.location, self.pokemon))
-        playerids = [row[0] for row in cur.fetchall()]
-
-        cur.execute("SELECT playerid FROM pmworldboss WHERE (location=? OR boss=?) AND comparator='|'",
-                    (self.location, self.pokemon))
-        playerids += [row[0] for row in cur.fetchall()]
-        conn.close()
-        self._pmrecipients = list(set(playerids))
