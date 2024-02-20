9323733e5400cc8edf9a8b47a2743d05dda9a0d8
kevin <kevin.plstr@gmail.com>
2024-02-20 21:48:23 +0100

    pagination

--- api/ingame_data/viewsets/eventviewset.py
+++ api/ingame_data/viewsets/eventviewset.py
@@ -7,3 +7,8 @@ from api.ingame_data.serializers.eventserializer import EventSerializer
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Event.objects.all().order_by("-time")
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        queryset = Event.objects.all().order_by("-time")
+
+        return queryset
